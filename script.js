a45062af0e19e30a711eae8552a9db378e87bd96
nsindittngcobo-wq <nsindittngcobo@gmail.com>
2026-04-11 02:27:18 +0200

    Added timezones

--- script.js
+++ script.js
@@ -11,5 +11,37 @@ durbanTimeElement.innerHTML = moment()
 let durbanDateElement = document.querySelector("#dbn-date");
 durbanDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
 }
+
+let cityInterval = null;
+
+function updateTime(event) {
+    let cityTimeZone = event.target.value;
+clearInterval(cityInterval);
+
+function showCityTime(){
+    if (!cityTimeZone) return;
+
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+   <div>
+    <div class="cityTime">
+    <div class="city"><h2>${cityName}</h2></div>
+ <div class="time">${cityTime.format("hh:mm:ss")}<small>AM</small></div>   
+</div>
+<div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+</div>
+</div>`;
+}
+
+showCityTime();
+cityInterval = setInterval(showCityTime, 1000);
+}
+
+let cityNameElement = document.querySelector("#city");
+cityNameElement.addEventListener("change", updateTime)
+
+
 displayTime();
 setInterval(displayTime, 1000);
